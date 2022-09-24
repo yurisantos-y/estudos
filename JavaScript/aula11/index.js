abbdb0052833f81d600b61b03006be6442872a72
Yuri <yuri01.sp@gmail.com>
2022-09-24 12:57:07 -0300

    Resultado com template string

--- JavaScript/aula11/index.js
+++ JavaScript/aula11/index.js
@@ -1,3 +1,19 @@
+// let num1 = prompt("Digite um numero:"); //Variavel que solicita um numero para o usuário
+// let num2 = prompt("Digite um numero:");//Variavel que solicita outro valor ao usuário
+// let resultado; // Variavel resultado
+
+// num1 = Number(num1); //Trocando o valor de num1 de string para Number
+// num2 = Number(num2); // Trocando o valor de num2 de string para Number
+
+// resultado = num1 + num2; // Aqui ele está fazendo com que a variavel resultado receba a soma de num1 + num2
+
+
+// alert('O resultado será: '+ resultado); // Mostrando para o usuário o resultado
+
+/*
+        Outro jeito
+*/
+
 let num1 = prompt("Digite um numero:"); //Variavel que solicita um numero para o usuário
 let num2 = prompt("Digite um numero:");//Variavel que solicita outro valor ao usuário
 let resultado; // Variavel resultado
@@ -8,7 +24,7 @@ num2 = Number(num2); // Trocando o valor de num2 de string para Number
 resultado = num1 + num2; // Aqui ele está fazendo com que a variavel resultado receba a soma de num1 + num2
 
 
-alert('O resultado será: '+ resultado); // Mostrando para o usuário o resultado
+alert(`O resultado será: ${resultado} `); // Mostrando para o usuário o resultado com template string
 
 /*const passo = 2;
 let contador = 1;
