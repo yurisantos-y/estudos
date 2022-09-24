c31d98147432332530b57f4092a173ed7486ce8e
Yuri <yuri01.sp@gmail.com>
2022-09-24 12:43:19 -0300

    Exercicio de soma no console

--- JavaScript/aula11/index.js
+++ JavaScript/aula11/index.js
@@ -1,9 +1,17 @@
-let num1 = prompt("Digite um numero:");
-let num2 = prompt("Digite um numero:");
-let resultado;
+let num1 = prompt("Digite um numero:"); //Variavel que solicita um numero para o usuário
+let num2 = prompt("Digite um numero:");//Variavel que solicita outro valor ao usuário
+let resultado; // Variavel resultado
 
-num1 = Number;
-num2 = Number;
+num1 = Number(num1); //Trocando o valor de num1 de string para Number
+num2 = Number(num2); // Trocando o valor de num2 de string para Number
+
+resultado = num1 + num2; // Aqui ele está fazendo com que a variavel resultado receba a soma de num1 + num2
 
 
-alert('O resultado será: ', resultado);
+alert('O resultado será: '+ resultado); // Mostrando para o usuário o resultado
+
+/*const passo = 2;
+let contador = 1;
+
+contador = contador + passo;
+console.log(contador);*/
