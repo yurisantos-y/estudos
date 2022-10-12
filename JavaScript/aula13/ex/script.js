19899b16cfa6ffdd27d40e8046ccb8d7552bca34
Yuri <yuri01.sp@gmail.com>
2022-10-12 16:36:51 -0300

    Number
    Estudando number em JS

--- JavaScript/aula13/ex/script.js
+++ JavaScript/aula13/ex/script.js
@@ -1,12 +1,12 @@
-let nome = prompt('Digite seu nome: ');
-document.body.innerHTML = `Seu nome é: ${nome}<br/>`;
-document.body.innerHTML += `seu nome tem: ${nome.length} letras<br/>`;
-document.body.innerHTML += `A segunda letra do seu nome é: ${nome.charAt(1)}<br/>`;
-document.body.innerHTML += `Qual o primeiro índice da letra n no seu nome? ${nome.indexOf('n')}<br/>`;
-document.body.innerHTML += `Qual o último índice da letra n no seu nome? ${nome.lastIndexOf('n')}<br/>`;
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(18)}<br/>`;
-document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br/>`;
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br/>`;
-document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br/>`;
+let nome = prompt('Digite seu nome: '); // Declarei a variavel nome
+document.body.innerHTML = `Seu nome é: ${nome}<br/>`; // Vai mostrar a variavel nome
+document.body.innerHTML += `seu nome tem: ${nome.length} letras<br/>`; //Quantidade de letras
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome.charAt(1)}<br/>`; //Mostra a letra que esta localizada na posição 1 da frase
+document.body.innerHTML += `Qual o primeiro índice da letra n no seu nome? ${nome.indexOf('n')}<br/>`; // Mostra o primeiro indice do meu nome com base em uma letra
+document.body.innerHTML += `Qual o último índice da letra n no seu nome? ${nome.lastIndexOf('n')}<br/>`; //Mostra o ultimo indice do nome com base em uma letra pré definida
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(18)}<br/>`; //Ultimas letras do nome
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br/>`; // Mostra todas as letras do nome
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br/>`; // Deixa em CAIXA ALTA
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br/>`; //Deixa em letra minusculas
 
 //Yuri dos Santos Pinto
---
+++ JavaScript/aula14/index.js
@@ -0,0 +1,10 @@
+let num1 = 0.7;
+let num2 = 0.3;
+
+num1 += num2; //num1 = num1 + num2
+
+//num1 = parseFloat(num1.toFixed(2));
+num1 = num1.toFixed(1)
+console.log(num1);  
+
+console.log(num1.toString(2)); // Mostra o codigo em binário de um numero
