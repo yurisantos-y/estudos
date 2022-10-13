5a3a30784034200f28078e2609b2a798c0cf73d0
Yuri <yuri01.sp@gmail.com>
2022-10-13 10:25:44 -0300

    Comentando Number

--- JavaScript/aula14/index.js
+++ JavaScript/aula14/index.js
@@ -3,8 +3,13 @@ let num2 = 0.3;
 
 num1 += num2; //num1 = num1 + num2
 
-//num1 = parseFloat(num1.toFixed(2));
-num1 = num1.toFixed(1)
-console.log(num1);  
+num1 = parseFloat(num1.toFixed(2));
+num1 = num1.toFixed(1);
+console.log(num1);
+
+console.log(num1.toString() + num2); // Transforma temporariamente a variavel em uma string
+num1 = num1.toString(); // Transforma realmente a variavel do tipo number em uma string
+console.log(num1.toFixed(2)); // Determina a quantidade de casas decimais que o numero vai ser arredondado
+console.log(Number.isInteger(num1)); //Verifica se a variavel é ou não do tipo string
 
 console.log(num1.toString(2)); // Mostra o codigo em binário de um numero
