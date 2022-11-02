59f9d1a9a6a43819ebde88a2ac164d6d0388d47d
Yuri <yuri01.sp@gmail.com>
2022-11-02 01:42:46 -0300

    function
    Estudando sobre function

---
+++ JavaScript/aula16/index.js
@@ -0,0 +1,21 @@
+/*function saudacoes(nome){ //Declaro a funcao saudacoes que recebe como parametro a variavel nome
+    console.log(`Bom dia ${nome}`); // Faço a msg Bom dia (nome) aparecer no console
+}
+
+saudacoes('Yuri'); //Chamo saudacoes e acrescento algum nome
+saudacoes('Paola'); //Chamo saudacoes e acrescento algum nome*/
+
+/*function saudacoes(nome){
+    return `Boa noite ${nome}!`;
+}
+
+const variavel = saudacoes('Yuri');
+console.log(variavel);*/
+
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+}
+
+
+console.log(soma(2, 2));
