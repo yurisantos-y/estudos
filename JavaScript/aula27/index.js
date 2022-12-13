1b96f36bb4675b7ac866357cb35f94280783a1ec
Yuri <yuri01.sp@gmail.com>
2022-12-13 09:48:17 -0300

    Operacao Ternaria

---
+++ JavaScript/aula27/index.js
@@ -0,0 +1,9 @@
+/* const pontuacao = 1000;
+const nivelUsuario = pontuacao >= 1000 ? 'Usuário Vip' : 'Usuário Normal';
+
+console.log(nivelUsuario);*/
+
+const resultado = 2000;
+const pontuacao = resultado >= 1000 ? 'Passou' : 'Não Passou';
+
+console.log(pontuacao);
