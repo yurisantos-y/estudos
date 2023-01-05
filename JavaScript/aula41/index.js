ce6bed97f492da1a27d74053b7e6589ea7833677
Yuri <yuri01.sp@gmail.com>
2023-01-05 19:56:29 -0300

    Exercicio lógica de prog - 01

---
+++ JavaScript/aula41/index.js
@@ -0,0 +1,15 @@
+//Escreva uma função que receba dois numeros e retorne o maior deles
+
+
+/* function maiorNumero (num1, num2) {
+    if (num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+};
+
+console.log(maiorNumero(1, 2)); */
+
+const max = (x, y) => x > y ? x : y;
+console.log(10, 5)
