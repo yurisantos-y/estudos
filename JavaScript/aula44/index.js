fc249c655fe46b03dd9e89532e44755680980e41
Yuri <yuri01.sp@gmail.com>
2023-01-07 12:45:06 -0300

    Tratando e lançando erros (Try, Catch, Throw. Finally)

---
+++ JavaScript/aula44/index.js
@@ -0,0 +1,9 @@
+
+
+try {
+    console.log(naoExiste)
+} catch (error) {
+    console.log(`A variavel não existe!`)
+    console.log(error)
+};
+
---
+++ JavaScript/exNodeJs/index.js
@@ -0,0 +1,39 @@
+// 7. Crie uma função que calcule a média de três números e retorne o resultado.
+
+/* function media (n1, n2, n3) {
+    const resultado = (n1 + n2 + n3) / 3;
+    return resultado;
+};
+
+console.log(media(7, 9, 10)); */
+
+// 8. Crie uma função que verifique se um número é par ou ímpar e retorne uma mensagem de acordo.
+
+/* function parImpar (numero) {
+    if (numero % 2 === 0){
+        return 'O numero é Par';
+    } else {
+        return 'O numero é par';
+    }
+    return numero;
+};
+
+console.log(parImpar(3)); */
+
+// 9. Crie uma função que verifique se um número é primo ou não e retorne uma mensagem de acordo.
+
+/* function primo (numero) {
+    if (numero < 2) {
+        return 'O numero não é primo';
+    }
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            return 'O numero não é primo';
+        }
+    }
+    return 'O numero é primo'
+};
+
+console.log(primo(86)); */
+
+// 
