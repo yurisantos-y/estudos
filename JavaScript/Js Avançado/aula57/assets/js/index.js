677db77a90239cb2c47cc2bb6ff25421bfd7b922
Yuri <yuri01.sp@gmail.com>
2023-05-08 15:18:35 -0300

    commit maio

---
+++ JavaScript/Js Avançado/aula57/assets/js/index.js
@@ -0,0 +1,28 @@
+function criaCalculadora() {
+    return {
+
+        display: document.querySelector('.display'),
+
+        inicia () {
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', function(e) {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+            }.bind(this));
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        },
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
