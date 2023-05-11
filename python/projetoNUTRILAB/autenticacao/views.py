9575c8906fed2191fa913bafa7c0dcd6380a7a78
Yuri <yuri01.sp@gmail.com>
2023-05-11 11:58:08 -0300

    finalizando cadastro

--- python/projetoNUTRILAB/autenticacao/views.py
+++ python/projetoNUTRILAB/autenticacao/views.py
@@ -35,7 +35,7 @@ def cadastro(request):
             ativacao.save()
 
             path_template = os.path.join(settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
-            email_html(path_template, 'Cadastro confirmado', [email,], username=username)
+            email_html(path_template, 'Cadastro confirmado', [email,], username=username, link_ativacao=f"127.0.0.1:8000/auth/ativar_conta/{token}")
             messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
             return redirect ('/auth/logar')
         except:
