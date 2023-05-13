4308e81bdea84efaef16860988f84b7befa2d24a
Yuri <yuri01.sp@gmail.com>
2023-05-13 10:06:02 -0300

    calculadora factory functions

--- JavaScript/Js Avançado/aula57/assets/js/index.js
+++ JavaScript/Js Avançado/aula57/assets/js/index.js
@@ -5,16 +5,62 @@ function criaCalculadora() {
 
         inicia () {
             this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        realizaConta() {
+            let conta = this.display.value
+
+            try {
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert('Conta inválida!');
+                    return;
+                }
+                this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta inválida!');
+                return;
+            }
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
         },
 
         cliqueBotoes() {
-            document.addEventListener('click', function(e) {
+            document.addEventListener('click', e => {
                 const el = e.target;
 
                 if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
+                    this.btnParaDisplay(el.textContent);
+                }   
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
                 }
-            }.bind(this));
+
+                if (el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eqt')) {
+                    this.realizaConta();
+                }
+            });
         },
 
         btnParaDisplay(valor) {
