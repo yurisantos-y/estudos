e45d2ee9ce935f2e620c0698e36aafa1e919faac
Yuri <yuri01.sp@gmail.com>
2023-06-01 11:58:43 -0300

    Finalizando primeira parte do projeto

--- python/projetoNUTRILAB/autenticacao/views.py
+++ python/projetoNUTRILAB/autenticacao/views.py
@@ -59,7 +59,7 @@ def logar(request):
             return redirect('/auth/logar')
         else:
             auth.login(request, usuario)
-            return redirect('/')
+            return redirect('/pacientes')
 
 def sair(request):
     auth.logout(request)
