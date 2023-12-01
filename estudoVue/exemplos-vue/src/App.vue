74590312f11ddf81ba7431df0167539d87a3a643
Yuri <yuri01.sp@gmail.com>
2023-12-01 01:38:33 -0300

    Diretivas

--- estudoVue/exemplos-vue/src/App.vue
+++ estudoVue/exemplos-vue/src/App.vue
@@ -1,26 +1,62 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- <template>
+    <div>
+        <h1>Hello, {{ nome }}</h1>
+        <input type="text" v-model="nome">
+        <br>
+        <a :href="link">Acesse o curso Vue</a>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        data: () => ({
+            nome: "yuri",
+            link: 'https://treinamento.vuejsbrasil.org'
+        })
+    }
+</script> -->
 
+
+<template>
+  <div>
+    <h1>Minha Lista de Tasks!</h1>
+
+    <button @click="() => showList = !showList">
+        Ver a lista
+    </button>
+
+    <br>
+
+    <input type="text" v-focus>
+
+    <ul v-if="showList">
+        <li v-for="(task, index) in tasks" :key="`${task}-${index}`">
+            {{ task.name }}
+        </li>
+    </ul>
+    <p v-else> Listas de tarefas escondidas</p>
+  </div>
+</template>
+
+<script>
+const focus = {
+    inserted: (el) => {
+        el.focus()
+    }
+}
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    directives: {
+        focus,
+    },
+    data: () => ({
+        showList: false,
+        tasks: [
+            { name: 'Fazer o curso', isDone: false }
+        ]
+    })
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
