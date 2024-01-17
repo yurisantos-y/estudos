74efdb239a7301f46a4b1cab08e8ebc6109cbed9
Yuri <yuri01.sp@gmail.com>
2024-01-17 01:54:10 -0300

    Metodo Splice

---
+++ JavaScript/Js Avançado/aula63/index.js
@@ -0,0 +1,6 @@
+const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+
+//nomes.splice(índice, delete, elem1, elem2, elem3);
+//pop
+const removidos = nomes.splice(-1, 2);
+console.log(nomes, removidos);
